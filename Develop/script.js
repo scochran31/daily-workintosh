cb57bdcdbb3746e161dcb573d35aa510cc334e73
Shawn Cochran <shawn.cochran31@gmail.com>
2021-04-15 20:01:54 -0600

    begin script

--- Develop/script.js
+++ Develop/script.js
@@ -1,3 +1,9 @@
 $(document).ready(function() {
     $('#currentDay').text(moment().format('dddd, MMMM Do YYYY'));
 });
+
+var currentDay = moment().format('dddd, MMMM Do YYYY');
+    console.log(currentDay);
+
+var hour = moment().format('h');
+    console.log(hour);
