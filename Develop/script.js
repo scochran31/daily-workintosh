5ededf56291765a53d35c5d134108392c1c09140
Shawn Cochran <shawn.cochran31@gmail.com>
2021-04-18 14:04:17 -0600

    create text area

--- Develop/script.js
+++ Develop/script.js
@@ -2,14 +2,6 @@ $(document).ready(function() {
     $('#currentDay').text(moment().format('dddd, MMMM Do YYYY'));
 });
 
-var currentDay = moment().format('dddd, MMMM Do YYYY');
-    console.log(currentDay);
+var scheduleEl = document.createElement('textarea');
+$('.col-sm-9').append(scheduleEl);
 
-var hour = moment().format('h');
-    if (hours >= 9 && hour <= 5) {
-        $('.schedule').addClass('present');
-    } else if(hours<9) {
-        $('.schedule').addClass('past');
-    } else {
-        $('.schedule').addClass('future');
-    }
