62d523e600817587b9736bb889466bb52060154b
Shawn Cochran <shawn.cochran31@gmail.com>
2021-04-18 21:37:03 -0600

    calendar functionality

--- Develop/script.js
+++ Develop/script.js
@@ -2,19 +2,36 @@ $(document).ready(function() {
     $('#currentDay').text(moment().format('dddd, MMMM Do YYYY'));
 });
 
-var scheduleEl = document.createElement('textarea');
-$('.col-sm-9').append(scheduleEl);
 
-var newSchedule = new Date();
-console.log(newSchedule);
-    var currentTime = newSchedule.getHours();
-    console.log(currentTime);
-    var thisRow = document.getElementsByName('time');
-    if (currentTime === thisRow) {
-        $('.col-sm-9').addClass('present')
-    } else if (currentTime < 9) {
-        $('.col-sm-9').addClass('past')
-    } else {
-        $('.col-sm-9').addClass('future');
-    };
+$('.saveBtn').on('click', function() {
+    var schedule = $(this)
+    .siblings('.description')
+    .val();
+
+    var save = $(this)
+    .siblings('.description')
+    .attr('id')
     
+    localStorage.setItem(save, schedule);
+    
+})
+
+function checkTime(){
+    
+    for (i = 9; i < 18; i++){
+       
+        if (moment().format('H') > i){
+            $(`#${i}`).addClass('past');
+        } else if (moment().format('H') < i){
+            $(`#${i}`).addClass('future');
+        } else{
+            $(`#${i}`).addClass('present');
+        };
+
+        $(`#${i}`)
+            .val(localStorage.getItem(i));
+
+    }
+};
+
+checkTime();
