787c74d911b4111a89fcae1d708d82b5733cdd0c
Shawn Cochran <shawn.cochran31@gmail.com>
2021-04-18 20:44:53 -0600

    calendar refactor

--- Develop/script.js
+++ Develop/script.js
@@ -5,3 +5,16 @@ $(document).ready(function() {
 var scheduleEl = document.createElement('textarea');
 $('.col-sm-9').append(scheduleEl);
 
+var newSchedule = new Date();
+console.log(newSchedule);
+    var currentTime = newSchedule.getHours();
+    console.log(currentTime);
+    var thisRow = document.getElementsByName('time');
+    if (currentTime === thisRow) {
+        $('.col-sm-9').addClass('present')
+    } else if (currentTime < 9) {
+        $('.col-sm-9').addClass('past')
+    } else {
+        $('.col-sm-9').addClass('future');
+    };
+    
